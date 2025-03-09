924c28e66d6b7cbeba63a5273489de26cb7de264
Matt Krueger <mkrueger@rstms.net>
2025-03-08 23:13:30 -0700

    add User, Request fields to Response base structure modify handlers to set User and Request fields in responses parse email address from VCARD in addresses handler change path of DELETE address endpoint

--- main.go
+++ main.go
@@ -41,6 +41,8 @@ var (
 
 type Response struct {
 	Success bool
+	User    string
+	Request string
 	Message string
 }
 
@@ -72,16 +74,16 @@ type PasswordResponse struct {
 func MAB(w http.ResponseWriter) (*api.Controller, bool) {
 	api, err := api.NewAddressBookController()
 	if err != nil {
-		fail(w, fmt.Sprintf("api init failed: %v", err), http.StatusInternalServerError)
+		fail(w, "system", "address book controller", fmt.Sprintf("api init failed: %v", err), http.StatusInternalServerError)
 		return nil, false
 	}
 	return api, true
 }
 
-func fail(w http.ResponseWriter, message string, status int) {
+func fail(w http.ResponseWriter, user, request, message string, status int) {
 	log.Printf("  [%d] %s", status, message)
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(Response{false, message})
+	json.NewEncoder(w).Encode(Response{User: user, Request: request, Success: false, Message: message})
 }
 
 func succeed(w http.ResponseWriter, message string, result interface{}) {
@@ -105,39 +107,41 @@ func checkClientCert(w http.ResponseWriter, r *http.Request) bool {
 	}
 	usernameHeader, ok := r.Header["X-Client-Cert-Dn"]
 	if !ok {
-		fail(w, "missing client cert DN", http.StatusBadRequest)
+		fail(w, "system", "client certificate check", "missing client cert DN", http.StatusBadRequest)
 		return false
 	}
 	if Verbose {
 		log.Printf("client cert dn: %s\n", usernameHeader[0])
 	}
 	if usernameHeader[0] != "CN=filterctl" {
-		fail(w, fmt.Sprintf("client cert (%s) != filterctl", usernameHeader[0]), http.StatusBadRequest)
+		fail(w, "system", "client certificate check", fmt.Sprintf("client cert (%s) != filterctl", usernameHeader[0]), http.StatusBadRequest)
 		return false
 	}
 	return true
 }
 
-func readConfig(w http.ResponseWriter) (*classes.SpamClasses, bool) {
+func readConfig(w http.ResponseWriter, user, request string) (*classes.SpamClasses, bool) {
 	config, err := classes.New(configFile)
 	if err != nil {
-		fail(w, "configuration read failed", http.StatusInternalServerError)
+		fail(w, user, request, "configuration read failed", http.StatusInternalServerError)
 		return nil, false
 	}
 	return config, true
 }
 
-func writeConfig(w http.ResponseWriter, config *classes.SpamClasses) bool {
+func writeConfig(w http.ResponseWriter, config *classes.SpamClasses, user, request string) bool {
 	err := config.Write(configFile)
 	if err != nil {
-		fail(w, "configuration write failed", http.StatusInternalServerError)
+		fail(w, user, request, "configuration write failed", http.StatusInternalServerError)
 		return false
 	}
 	return true
 }
 
-func sendClasses(w http.ResponseWriter, config *classes.SpamClasses, address string) {
+func sendClasses(w http.ResponseWriter, config *classes.SpamClasses, address, request string) {
 	response := ClassesResponse{}
+	response.User = address
+	response.Request = request
 	response.Success = true
 	response.Message = fmt.Sprintf("%s spam classes", address)
 	response.Classes = config.GetClasses(address)
@@ -150,17 +154,20 @@ func handleGetClass(w http.ResponseWriter, r *http.Request) {
 	}
 	address := r.PathValue("address")
 	scoreParam := r.PathValue("score")
+	requestString := fmt.Sprintf("classify %v", scoreParam)
 	if Verbose {
 		log.Printf("GET address=%s score=%s\n", address, scoreParam)
 	}
 	score, err := strconv.ParseFloat(scoreParam, 32)
 	if err != nil {
-		fail(w, "score conversion failed", http.StatusBadRequest)
+		fail(w, address, requestString, "score conversion failed", http.StatusBadRequest)
 		return
 	}
-	config, ok := readConfig(w)
+	config, ok := readConfig(w, address, requestString)
 	if ok {
 		var response ClassResponse
+		response.User = address
+		response.Request = requestString
 		response.Success = true
 		response.Class = config.GetClass([]string{address}, float32(score))
 		response.Message = fmt.Sprintf("%v", response.Class)
@@ -173,12 +180,13 @@ func handleGetClasses(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	address := r.PathValue("address")
+	requestString := "get classes"
 	if Verbose {
 		log.Printf("GET address=%s\n", address)
 	}
-	config, ok := readConfig(w)
+	config, ok := readConfig(w, address, requestString)
 	if ok {
-		sendClasses(w, config, address)
+		sendClasses(w, config, address, requestString)
 	}
 }
 
@@ -189,21 +197,22 @@ func handlePutClassThreshold(w http.ResponseWriter, r *http.Request) {
 	address := r.PathValue("address")
 	name := r.PathValue("name")
 	threshold := r.PathValue("threshold")
+	requestString := fmt.Sprintf("set class %s threshold to %v", name, threshold)
 	if Verbose {
 		log.Printf("PUT address=%s name=%s threshold=%s\n", address, name, threshold)
 	}
 	score, err := strconv.ParseFloat(threshold, 32)
 	if err != nil {
-		fail(w, "threshold conversion failed", http.StatusBadRequest)
+		fail(w, address, requestString, "threshold conversion failed", http.StatusBadRequest)
 		return
 	}
-	config, ok := readConfig(w)
+	config, ok := readConfig(w, address, requestString)
 	if !ok {
 		return
 	}
 	config.SetThreshold(address, name, float32(score))
-	if writeConfig(w, config) {
-		sendClasses(w, config, address)
+	if writeConfig(w, config, address, requestString) {
+		sendClasses(w, config, address, requestString)
 	}
 }
 
@@ -212,17 +221,18 @@ func handleDeleteUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	address := r.PathValue("address")
+	requestString := "delete user"
 	if Verbose {
 		log.Printf("DELETE (user) address=%s\n", address)
 	}
-	config, ok := readConfig(w)
+	config, ok := readConfig(w, address, requestString)
 	if !ok {
 		return
 	}
 	config.DeleteClasses(address)
-	if writeConfig(w, config) {
+	if writeConfig(w, config, address, requestString) {
 		message := "user deleted"
-		succeed(w, message, &Response{Success: true, Message: message})
+		succeed(w, message, &Response{User: address, Request: requestString, Success: true, Message: message})
 	}
 }
 
@@ -232,17 +242,18 @@ func handleDeleteClass(w http.ResponseWriter, r *http.Request) {
 	}
 	address := r.PathValue("address")
 	name := r.PathValue("name")
+	requestString := fmt.Sprintf("delete class %s", name)
 	if Verbose {
 		log.Printf("DELETE (class) address=%s name=%s\n", address, name)
 	}
-	config, ok := readConfig(w)
+	config, ok := readConfig(w, address, requestString)
 	if !ok {
 		return
 	}
 	config.GetClasses(address)
 	config.DeleteClass(address, name)
-	if writeConfig(w, config) {
-		sendClasses(w, config, address)
+	if writeConfig(w, config, address, requestString) {
+		sendClasses(w, config, address, requestString)
 	}
 }
 
@@ -251,6 +262,7 @@ func handleListBooks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	user := r.PathValue("user")
+	requestString := "list books"
 	if Verbose {
 		log.Printf("GetBooks: user=%s\n", user)
 	}
@@ -261,7 +273,7 @@ func handleListBooks(w http.ResponseWriter, r *http.Request) {
 	}
 	apiResponse, err := mab.GetBooks(user)
 	if err != nil {
-		fail(w, fmt.Sprintf("api GetBooks failed: %v", err), http.StatusInternalServerError)
+		fail(w, user, requestString, fmt.Sprintf("api GetBooks failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 
@@ -269,6 +281,8 @@ func handleListBooks(w http.ResponseWriter, r *http.Request) {
 		log.Printf("response: %+v\n", apiResponse)
 	}
 	var response BooksResponse
+	response.Request = requestString
+	response.User = user
 	response.Success = true
 	response.Message = apiResponse.Message
 	response.Books = make([]string, len(apiResponse.Books))
@@ -293,7 +307,7 @@ func handleAddBook(w http.ResponseWriter, r *http.Request) {
 	var request CreateBookRequest
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		fail(w, fmt.Sprintf("failed encoding request: %v", err), http.StatusBadRequest)
+		fail(w, "system", "create book", fmt.Sprintf("failed encoding request: %v", err), http.StatusBadRequest)
 		return
 	}
 	mab, ok := MAB(w)
@@ -303,15 +317,16 @@ func handleAddBook(w http.ResponseWriter, r *http.Request) {
 	if Verbose {
 		log.Printf("AddBook: user=%s name=%s description=%s\n", request.Username, request.Bookname, request.Description)
 	}
+	requestString := fmt.Sprintf("create book %s", request.Bookname)
 	response, err := mab.AddBook(request.Username, request.Bookname, request.Description)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.AddBook failed: %v", err), http.StatusInternalServerError)
+		fail(w, request.Username, requestString, fmt.Sprintf("api.AddBook failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", response)
 	}
-	succeed(w, response.Message, &Response{Message: response.Message, Success: true})
+	succeed(w, response.Message, &Response{User: request.Username, Request: requestString, Message: response.Message, Success: true})
 	return
 
 }
@@ -325,19 +340,20 @@ func handleDeleteBook(w http.ResponseWriter, r *http.Request) {
 	if Verbose {
 		log.Printf("DeleteBook: username=%s bookname=%s\n", username, bookname)
 	}
+	requestString := fmt.Sprintf("delete book %s", bookname)
 	mab, ok := MAB(w)
 	if !ok {
 		return
 	}
 	response, err := mab.DeleteBook(username, bookname)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.DeleteBook failed: %v", err), http.StatusInternalServerError)
+		fail(w, username, requestString, fmt.Sprintf("api.DeleteBook failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", response)
 	}
-	succeed(w, response.Message, &Response{Message: response.Message, Success: true})
+	succeed(w, response.Message, &Response{User: username, Request: requestString, Message: response.Message, Success: true})
 }
 
 func handleAddAddress(w http.ResponseWriter, r *http.Request) {
@@ -353,9 +369,10 @@ func handleAddAddress(w http.ResponseWriter, r *http.Request) {
 	var request AddAddressRequest
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		fail(w, fmt.Sprintf("failed encoding request: %v", err), http.StatusBadRequest)
+		fail(w, "system", "add address", fmt.Sprintf("failed encoding request: %v", err), http.StatusBadRequest)
 		return
 	}
+	requestString := fmt.Sprintf("add %s to %s", request.Address, request.Bookname)
 	if Verbose {
 		log.Printf("AddAddress: username=%s bookname=%s address=%s name=%s\n", request.Username, request.Bookname, request.Address, request.Name)
 	}
@@ -365,13 +382,13 @@ func handleAddAddress(w http.ResponseWriter, r *http.Request) {
 	}
 	response, err := mab.AddAddress(request.Username, request.Bookname, request.Address, request.Name)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.AddAddress failed: %v", err), http.StatusInternalServerError)
+		fail(w, request.Username, requestString, fmt.Sprintf("api.AddAddress failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", response)
 	}
-	succeed(w, response.Message, &Response{Message: response.Message, Success: true})
+	succeed(w, response.Message, &Response{User: request.Username, Request: requestString, Message: response.Message, Success: true})
 	return
 }
 
@@ -382,6 +399,7 @@ func handleDeleteAddress(w http.ResponseWriter, r *http.Request) {
 	username := r.PathValue("user")
 	bookname := r.PathValue("book")
 	address := r.PathValue("address")
+	requestString := fmt.Sprintf("delete %s from %s", address, bookname)
 	if Verbose {
 		log.Printf("DeleteAddress: user=%s book=%s address=%s\n", username, bookname, address)
 	}
@@ -391,13 +409,13 @@ func handleDeleteAddress(w http.ResponseWriter, r *http.Request) {
 	}
 	response, err := mab.DeleteAddress(username, bookname, address)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.DeleteAddress failed: %v", err), http.StatusInternalServerError)
+		fail(w, username, requestString, fmt.Sprintf("api.DeleteAddress failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", response)
 	}
-	succeed(w, response.Message, &Response{Message: response.Message, Success: true})
+	succeed(w, response.Message, &Response{User: username, Request: requestString, Message: response.Message, Success: true})
 	return
 }
 
@@ -407,6 +425,7 @@ func handleListAddresses(w http.ResponseWriter, r *http.Request) {
 	}
 	username := r.PathValue("user")
 	bookname := r.PathValue("book")
+	requestString := fmt.Sprintf("list %s addresses", bookname)
 	if Verbose {
 		log.Printf("ListAddresses: user=%s book=%s\n", username, bookname)
 	}
@@ -416,18 +435,25 @@ func handleListAddresses(w http.ResponseWriter, r *http.Request) {
 	}
 	apiResponse, err := mab.Addresses(username, bookname)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.Addresses failed: %v", err), http.StatusInternalServerError)
+		fail(w, username, requestString, fmt.Sprintf("api.Addresses failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", apiResponse)
 	}
 	var response AddressesResponse
+	response.User = username
+	response.Request = requestString
 	response.Success = true
 	response.Message = apiResponse.Message
 	response.Addresses = make([]string, len(apiResponse.Addresses))
 	for i, addr := range apiResponse.Addresses {
-		response.Addresses[i] = fmt.Sprintf("%v", addr)
+		field := addr.Card.Get("EMAIL")
+		if field == nil {
+			fail(w, username, requestString, fmt.Sprintf("failed decoding EMAIL field from VCARD: %+v", addr.Card), http.StatusInternalServerError)
+			return
+		}
+		response.Addresses[i] = field.Value
 	}
 	succeed(w, response.Message, &response)
 }
@@ -439,6 +465,7 @@ func handleScanAddress(w http.ResponseWriter, r *http.Request) {
 	}
 	username := r.PathValue("user")
 	address := r.PathValue("address")
+	requestString := fmt.Sprintf("scan books for %s", address)
 	if Verbose {
 		log.Printf("ScanAddress: user=%s address=%s\n", username, address)
 	}
@@ -448,13 +475,15 @@ func handleScanAddress(w http.ResponseWriter, r *http.Request) {
 	}
 	apiResponse, err := mab.ScanAddress(username, address)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.ScanAddress failed: %v", err), http.StatusInternalServerError)
+		fail(w, username, requestString, fmt.Sprintf("api.ScanAddress failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", apiResponse)
 	}
 	var response BooksResponse
+	response.User = username
+	response.Request = requestString
 	response.Success = true
 	response.Message = apiResponse.Message
 	response.Books = make([]string, len(apiResponse.Books))
@@ -469,6 +498,7 @@ func handlePasswordRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	username := r.PathValue("user")
+	requestString := "password lookup"
 	if Verbose {
 		log.Printf("PasswordRequest: user=%s\n", username)
 	}
@@ -478,17 +508,19 @@ func handlePasswordRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	apiResponse, err := mab.GetPassword(username)
 	if err != nil {
-		fail(w, fmt.Sprintf("api.GetPassword failed: %v", err), http.StatusInternalServerError)
+		fail(w, username, requestString, fmt.Sprintf("api.GetPassword failed: %v", err), http.StatusInternalServerError)
 		return
 	}
 	if Verbose {
 		log.Printf("response: %v\n", apiResponse)
 	}
 	if !apiResponse.Success {
-		fail(w, apiResponse.Message, 404)
+		fail(w, username, requestString, apiResponse.Message, 404)
 		return
 	}
 	var response PasswordResponse
+	response.User = username
+	response.Request = requestString
 	response.Success = true
 	response.Message = fmt.Sprintf("%s password", username)
 	response.Password = apiResponse.Password
@@ -515,7 +547,7 @@ func runServer(addr *string, port *int) {
 	http.HandleFunc("POST /filterctl/book/", handleAddBook)
 	http.HandleFunc("POST /filterctl/address/", handleAddAddress)
 	http.HandleFunc("DELETE /filterctl/book/{user}/{book}/", handleDeleteBook)
-	http.HandleFunc("DELETE /filterctl/book/{user}/{book}/{address}/", handleDeleteAddress)
+	http.HandleFunc("DELETE /filterctl/address/{user}/{book}/{address}/", handleDeleteAddress)
 
 	go func() {
 		log.Printf("%s v%s rspamd_classes=v%s mabctl_api=v%s, uid=%d gid=%d started as PID %d listening on %s\n", serverName, Version, classes.Version, api.Version, os.Getuid(), os.Getgid(), os.Getpid(), listen)
