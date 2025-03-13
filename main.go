8fcbc57b7b9437059f18b89f78d9dc3959e124f0
Matt Krueger <mkrueger@rstms.net>
2025-03-13 03:51:46 -0600

    Bump version: 0.3.8 → 0.3.9

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ const defaultConfigFile = "/etc/mail/filter_rspamd_classes.json"
 const defaultLogFile = "/var/log/filterctld.log"
 const defaultPort = 2016
 const SHUTDOWN_TIMEOUT = 5
-const Version = "0.3.8"
+const Version = "0.3.9"
 
 var Verbose bool
 var Debug bool
