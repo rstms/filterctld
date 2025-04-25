ed2bc37d70f53e8a20d857ebd91aa20133a132ca
Matt Krueger <mkrueger@rstms.net>
2025-04-25 15:13:26 -0600

    Bump version: 1.1.5 → 1.1.6

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ const defaultConfigFile = "/etc/mail/filter_rspamd_classes.json"
 const defaultLogFile = "/var/log/filterctld.log"
 const defaultPort = 2016
 const SHUTDOWN_TIMEOUT = 5
-const Version = "1.1.5"
+const Version = "1.1.6"
 
 var Verbose bool
 var Debug bool
